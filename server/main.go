f8f2a33a1c978161a90f34fb1b1a19b3ba72b2c1
maxnemoy <maxnemoy@gmail.com>
2022-03-10 10:10:50 +0800

    refactoring api

--- server/main.go
+++ server/main.go
@@ -10,8 +10,8 @@ import (
 	"github.com/maxnemoy/ilimjob_server/handlers/post"
 	"github.com/maxnemoy/ilimjob_server/handlers/post/postType"
 	"github.com/maxnemoy/ilimjob_server/handlers/vacancy"
-	"github.com/maxnemoy/ilimjob_server/handlers/tag"
-	"github.com/maxnemoy/ilimjob_server/handlers/category"
+	"github.com/maxnemoy/ilimjob_server/handlers/vacancy/tag"
+	"github.com/maxnemoy/ilimjob_server/handlers/vacancy/category"
 	"net/http"
 	"os"
 
@@ -53,10 +53,8 @@ func main() {
 	apiPublic.GET("/post/types", postType.GetAll(conn))
 
 	apiPublic.GET("/vacancies", vacancy.GetAll(conn))
-
-	apiPublic.GET("/tags", tag.GetAll(conn))
-
-	apiPublic.GET("/categories", category.GetAll(conn))
+	apiPublic.GET("/vacancy/tags", tag.GetAll(conn))
+	apiPublic.GET("/vacancy/categories", category.GetAll(conn))
 	
 
 	privateZone := apiPublic.Group("/v1")
@@ -75,12 +73,10 @@ func main() {
 
 	privateZone.PUT("/vacancy", vacancy.Create(conn))
 	privateZone.PATCH("/vacancy", vacancy.Update(conn))
-
-	privateZone.PUT("/tag", tag.Create(conn))
-	privateZone.PATCH("/tag", tag.Update(conn))
-
-	privateZone.PUT("/category", category.Create(conn))
-	privateZone.PATCH("/category", category.Update(conn))
+	privateZone.PUT("/vacancy/tag", tag.Create(conn))
+	privateZone.PATCH("/vacancy/tag", tag.Update(conn))
+	privateZone.PUT("/vacancy/category", category.Create(conn))
+	privateZone.PATCH("/vacancy/category", category.Update(conn))
 
 	apiPublic.Logger.Fatal(apiPublic.Start(":" + port))
 }
