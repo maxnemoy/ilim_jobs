64527960eed9cef359bd5c5d2615bf9b9e63f7d2
maxnemoy <maxnemoy@gmail.com>
2022-03-21 11:56:03 +0800

    switch to single body

--- server/db/models/vacancy.go
+++ server/db/models/vacancy.go
@@ -6,20 +6,17 @@ import (
 )
 
 type Vacancy struct {
-	ID               int       `pg:"id,pk" json:"id"`
-	CreateAt         time.Time `pg:"create_at" json:"create_at"`
-	UpgradeAt        time.Time `pg:"upgrade_at" json:"upgrade_at"`
-	DeleteAt         time.Time `pg:"delete_at" json:"delete_at"`
-	Published        bool      `pg:"published" json:"published"`
-	Title            string    `pg:"title" json:"title"`
-	Description      string    `pg:"description" json:"description"`
-	Responsibilities string    `pg:"responsibilities" json:"responsibilities"`
-	Requirements     string    `pg:"requirements" json:"requirements"`
-	Terms            string    `pg:"terms" json:"terms"`
-	Author           int       `pg:"author" json:"author"`
-	Contacts         []string    `pg:"contacts,array" json:"contacts"`
-	Tags             []int     `pg:"tags,array" json:"tags"`
-	Category         int       `pg:"category" json:"category"`
+	ID        int       `pg:"id,pk" json:"id"`
+	CreateAt  time.Time `pg:"create_at" json:"create_at"`
+	UpgradeAt time.Time `pg:"upgrade_at" json:"upgrade_at"`
+	DeleteAt  time.Time `pg:"delete_at" json:"delete_at"`
+	Published bool      `pg:"published" json:"published"`
+	Title     string    `pg:"title" json:"title"`
+	Body      string    `pg:"body" json:"body"`
+	Author    int       `pg:"author" json:"author"`
+	Contacts  []string  `pg:"contacts,array" json:"contacts"`
+	Tags      []int     `pg:"tags,array" json:"tags"`
+	Category  int       `pg:"category" json:"category"`
 }
 
 func (v *Vacancy) Create(conn *pg.DB) error {
@@ -34,10 +31,7 @@ func (v *Vacancy) Upgrade(conn *pg.DB) error {
 	_, err := conn.Model(v).Where("id = ?0", v.ID).
 		Set("published = ?0", v.Published).
 		Set("title = ?0", v.Title).
-		Set("description = ?0", v.Description).
-		Set("responsibilities = ?0", v.Responsibilities).
-		Set("requirements = ?0", v.Requirements).
-		Set("terms = ?0", v.Terms).
+		Set("body = ?0", v.Body).
 		Set("author = ?0", v.Author).
 		Set("contacts = ?0", pg.Array(v.Contacts)).
 		Set("tags = ?0", pg.Array(v.Tags)).
