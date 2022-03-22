f683d8214759a8d7df99e0a22825c1e2a054645f
maxnemoy <maxnemoy@gmail.com>
2022-03-22 12:30:59 +0800

    refactoring resumeviewer

--- ilimgroup_jobs/lib/pages/profile/profile_page.dart
+++ ilimgroup_jobs/lib/pages/profile/profile_page.dart
@@ -3,8 +3,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ilimgroup_jobs/config/singleton.dart';
 import 'package:ilimgroup_jobs/core/logic/authentication/cubit.dart';
 import 'package:ilimgroup_jobs/core/logic/authentication/repository.dart';
+import 'package:ilimgroup_jobs/core/logic/utils/file_uploader.dart';
 import 'package:ilimgroup_jobs/core/models/user/auth_data.dart';
+import 'package:ilimgroup_jobs/core/models/user/resume/resume_data.dart';
 import 'package:ilimgroup_jobs/pages/discover/discover_page.dart';
+import 'package:intl/intl.dart';
 import 'package:routemaster/routemaster.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -174,117 +177,198 @@ class FavoriteVacancy extends StatelessWidget {
   }
 }
 
-class ResumeViewer extends StatelessWidget {
+class ResumeViewer extends StatefulWidget {
   const ResumeViewer({Key? key}) : super(key: key);
 
   @override
+  State<ResumeViewer> createState() => _ResumeViewerState();
+}
+
+class _ResumeViewerState extends State<ResumeViewer> {
+  ResumeData resume = ResumeData();
+
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController lastNameController = TextEditingController();
+  final TextEditingController phoneNameController = TextEditingController();
+  final TextEditingController emailNameController = TextEditingController();
+  final TextEditingController cityNameController = TextEditingController();
+  final TextEditingController citizenshipController = TextEditingController();
+  final TextEditingController detailController = TextEditingController();
+
+  @override
+  void initState() {
+    initArraysIfNecessary();
+    super.initState();
+  }
+
+  void initArraysIfNecessary() {
+    if (resume.assets == null) {
+      resume = resume.copyWith(assets: []);
+    }
+    if (resume.works == null) {
+      resume = resume.copyWith(works: []);
+    }
+    if (resume.education == null) {
+      resume = resume.copyWith(education: []);
+    }
+    if (resume.categories == null) {
+      resume = resume.copyWith(categories: []);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(label: Text("Имя")),
-                ),
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(label: Text("Фамилия")),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(label: Text("Телефон")),
-                ),
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(label: Text("Город")),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(label: Text("Гражданство")),
-                ),
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(label: Text("Город")),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: Row(
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: SingleChildScrollView(
+        child: Center(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 800),
+            child: Column(
+              children: [
+                Row(
                   children: [
-                    Text(DateTime.now().toString()),
-                    ElevatedButton(
-                        onPressed: () {
-                          showDatePicker(
-                              context: context,
-                              initialDate: DateTime.now(),
-                              firstDate: DateTime(1970),
-                              lastDate: DateTime.now());
-                        },
-                        child: Text("Дата Рожения")),
+                    Expanded(
+                      child: TextField(
+                        controller: firstNameController,
+                        decoration: const InputDecoration(label: Text("Имя")),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        controller: lastNameController,
+                        decoration:
+                            const InputDecoration(label: Text("Фамилия")),
+                      ),
+                    ),
                   ],
                 ),
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-              Expanded(
-                child: Row(
+                Row(
                   children: [
-                    Text("Пол"),
-                    Spacer(),
-                    DropdownButton<String>(
-                        value: "Мужской",
-                        items: const [
-                          DropdownMenuItem(
-                            child: Text("Мужской"),
-                            value: "Мужской",
-                          ),
-                          DropdownMenuItem(
-                            child: Text("Женский"),
-                            value: "Женский",
-                          ),
+                    Expanded(
+                      child: TextField(
+                        controller: phoneNameController,
+                        decoration:
+                            const InputDecoration(label: Text("Телефон")),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        controller: emailNameController,
+                        decoration: const InputDecoration(
+                            label: Text("Электронная почта")),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: citizenshipController,
+                        decoration:
+                            const InputDecoration(label: Text("Гражданство")),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        controller: cityNameController,
+                        decoration: const InputDecoration(label: Text("Город")),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Row(
+                        children: [
+                          const Text("Дата рождения: "),
+                          if (resume.birthday == null)
+                            if (resume.birthday != null)
+                              Text(
+                                DateFormat('dd.MM.yyyy')
+                                    .format(resume.birthday!),
+                                style: Theme.of(context).textTheme.bodyText1,
+                              ),
+                          IconButton(
+                              onPressed: () {
+                                showDatePicker(
+                                    context: context,
+                                    initialDate: DateTime.now(),
+                                    firstDate: DateTime(1970),
+                                    lastDate: DateTime.now());
+                              },
+                              icon: const Icon(Icons.calendar_month)),
                         ],
-                        onChanged: (v) {}),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                      child: Row(
+                        children: [
+                          const Text("Пол: "),
+                          const Spacer(),
+                          DropdownButton<String>(
+                              value: resume.gender,
+                              items: const [
+                                DropdownMenuItem(
+                                  child: Text("Мужской"),
+                                  value: "Мужской",
+                                ),
+                                DropdownMenuItem(
+                                  child: Text("Женский"),
+                                  value: "Женский",
+                                ),
+                              ],
+                              onChanged: (v) {
+                                setState(() {
+                                  resume = resume.copyWith(gender: v);
+                                });
+                              }),
+                        ],
+                      ),
+                    )
                   ],
                 ),
-              )
-            ],
+                TextField(
+                  maxLines: null,
+                  decoration: const InputDecoration(
+                      label: Text("Дополнительная информация")),
+                  controller: detailController,
+                ),
+                MultiValuesField(
+                  list: resume.education!,
+                  title: "Обучение",
+                ),
+                MultiValuesField(
+                  list: resume.works!,
+                  title: "Предыдущие места работы",
+                ),
+                MultiFileField(
+                  list: resume.assets!,
+                  title: "Сертификаты и документы",
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: ElevatedButton(
+                      onPressed: () {}, child: const Text("Сохранить")),
+                )
+              ],
+            ),
           ),
-          TextField(
-            maxLines: null,
-            decoration: InputDecoration(label: Text("Дополнительная информация")),
-          ),
-          MultiValuesField(
-            list: [],
-          )
-        ],
+        ),
       ),
     );
   }
@@ -292,7 +376,10 @@ class ResumeViewer extends StatelessWidget {
 
 class MultiValuesField extends StatefulWidget {
   final List<String> list;
-  const MultiValuesField({Key? key, required this.list}) : super(key: key);
+  final String title;
+  const MultiValuesField(
+      {Key? key, required this.list, this.title = "some zone"})
+      : super(key: key);
 
   @override
   State<MultiValuesField> createState() => _MultiValuesFieldState();
@@ -303,22 +390,96 @@ class _MultiValuesFieldState extends State<MultiValuesField> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: List.from(widget.list.map((e) => ListTile(
-            title: Text(e),
-          )))
-        ..add(TextField(
-          controller: controller,
-          onEditingComplete: () {
-            setState(() {
-              widget.list.add(controller.text);
-            });
-          },
-          decoration: const InputDecoration(
-            label: Text("SomeLabel"),
-            suffixIcon: Icon(Icons.add),
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Column(
+        children: List.from(widget.list.map((e) => ListTile(
+              title: Text(e),
+              trailing: IconButton(
+                icon: Icon(
+                  Icons.delete,
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+                onPressed: () {
+                  setState(() {
+                    widget.list.removeWhere((element) => element == e);
+                  });
+                },
+              ),
+            )))
+          ..add(TextField(
+            controller: controller,
+            onEditingComplete: () {
+              setState(() {
+                widget.list.add(controller.text);
+                controller.clear();
+              });
+            },
+            decoration: InputDecoration(
+              label: Text(widget.title),
+              suffixIcon: const Icon(Icons.add),
+            ),
+          ))
+          ..insert(0, ZoneTitle(text: widget.title)),
+      ),
+    );
+  }
+}
+
+class MultiFileField extends StatefulWidget {
+  final List<String> list;
+  final String title;
+  const MultiFileField({Key? key, required this.list, this.title = "some zone"})
+      : super(key: key);
+
+  @override
+  State<MultiFileField> createState() => _MultiFileFieldState();
+}
+
+class _MultiFileFieldState extends State<MultiFileField> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+          Container(
+            height: 300,
+            child: GridView.count(
+              crossAxisCount: 3,
+              children: List.from(widget.list.map((e) => ListTile(
+                    title: Container(
+                      width: 100,
+                      height: 100,
+                      padding: const EdgeInsets.all(5),
+                      child: Image.network(e)),
+                    trailing: IconButton(
+                      icon: Icon(
+                        Icons.delete,
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          widget.list.removeWhere((element) => element == e);
+                        });
+                      },
+                    ),
+                  )))
+                // ..add(ElevatedButton.icon(
+                //     onPressed: () async {
+                //       String path = await pickFileAndUpload(
+                //           getIt<AuthenticationRepository>().auth?.token ?? "");
+                //       setState(() {
+                //         widget.list.add(path);
+                //       });
+                //     },
+                //     label: const Text("Добавить файл"),
+                //     icon: const Icon(Icons.attach_file)))
+                // ..insert(0, ZoneTitle(text: widget.title)),
+            ),
           ),
-        )),
+        ],
+      ),
     );
   }
 }
